55b213edeeb7c18e60b8dac40607d1957c1a24d5
Eu <karllhenning64@gmail.com>
2016-10-15 15:23:46 -0300

    Added Tree and Hash code for receiving input under 'entrada-*.txt' format

--- TrabHash/main.cpp
+++ TrabHash/main.cpp
@@ -32,6 +32,14 @@ void TreeMain();
 void HashMain();
 
 int main(int argc, char const *argv[]) {
+	// ListMain();
+	// TreeMain();
+	HashMain();
+
+	return 0;
+}
+
+void ListMain() {
 	list<NameValuePair> nameList;
 
 	int numNames;
@@ -67,30 +75,76 @@ int main(int argc, char const *argv[]) {
 			cout << it->value << endl;
 		}
 	}
-
-	// // Binary Tree Search
-	// map<const char*, int> nameTree;
-	// nameTree[name] = 20;
-	// cout << nameTree[name] << endl;
-	//
-	// // Hash with binary tree search for collision
-	// HashMapTree nameHashMap(10);
-	// nameHashMap.SetValue(name, 20);
-	// cout << nameHashMap.GetValue(name) << endl;
-
-	return 0;
-}
-
-void ListMain() {
-
 }
 
 void TreeMain() {
+	map<string, int> nameTree;
 
+	int numNames;
+	cin >> numNames;
+
+	while (numNames-- > 0) {
+		int id;
+		cin >> id;
+		scanf(" "); // "removes" the next whitespace from cin
+
+		char name[40];
+		cin.getline(name, sizeof(name));
+		// cout << id << ' ' << name << endl;
+		nameTree[name] = id;
+
+	}
+
+	int numQueries;
+	cin >> numQueries;
+	// cout << "NumQueries: " << numQueries << endl;
+
+	char clearLine[30];
+	cin.getline(clearLine, sizeof(clearLine));
+
+	while (numQueries > 0) {
+		numQueries -= 1;
+		char name[40];
+		cin.getline(name, sizeof(name));
+		// cout << numQueries << ": "<< name << endl;
+
+		cout << nameTree[name] << endl;
+	}
 }
 
 void HashMain() {
+		HashMapTree nameHashMap(10);
 
+		int numNames;
+		cin >> numNames;
+
+		while (numNames-- > 0) {
+			int id;
+			cin >> id;
+			scanf(" "); // "removes" the next whitespace from cin
+
+			char name[40];
+			cin.getline(name, sizeof(name));
+			// cout << id << ' ' << name << endl;
+			nameHashMap.SetValue(name, id);
+
+		}
+
+		int numQueries;
+		cin >> numQueries;
+		// cout << "NumQueries: " << numQueries << endl;
+
+		char clearLine[30];
+		cin.getline(clearLine, sizeof(clearLine));
+
+		while (numQueries > 0) {
+			numQueries -= 1;
+			char name[40];
+			cin.getline(name, sizeof(name));
+			// cout << numQueries << ": "<< name << endl;
+
+			cout << nameHashMap.GetValue(name) << endl;
+		}
 }
 
 
