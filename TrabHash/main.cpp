c0a31d36060c710b37990c47fd3e972d169f4c37
Eu <karllhenning64@gmail.com>
2016-10-15 15:12:46 -0300

    Program now receives an input following the format in 'entrada' files

--- TrabHash/main.cpp
+++ TrabHash/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <map>
 #include <list>
@@ -9,43 +10,89 @@
 
 using namespace std;
 
-void PrintHashes(string text);
 
 struct NameValuePair {
-	const char* name;
+	char name[40];
 	int value;
-	NameValuePair(const char* argName, int argValue) {
-		name = argName;
+
+	NameValuePair(const char* argName, const int argValue) {
+		strcpy(name, argName);
 		value = argValue;
 	}
 
 	inline bool operator==(const char* rhs){
 		return strcmp(name, rhs) == 0;
 	}
-
 };
 
+void PrintHashes(string text);
+
+void ListMain();
+void TreeMain();
+void HashMain();
+
 int main(int argc, char const *argv[]) {
-	const char * name = "Winnie";
-
-	// Linear List Search
 	list<NameValuePair> nameList;
-	nameList.push_back(NameValuePair(name, 20));
-	auto it = find(nameList.begin(), nameList.end(), name);
-	cout << it->value <<  endl;
 
-	// Binary Tree Search
-	map<const char*, int> nameTree;
-	nameTree[name] = 20;
-	cout << nameTree[name] << endl;
+	int numNames;
+	cin >> numNames;
 
-	// Hash with binary tree search for collision
-	HashMapTree nameHashMap(10);
-	nameHashMap.SetValue(name, 20);
-	cout << nameHashMap.GetValue(name) << endl;
+	while (numNames-- > 0) {
+		int id;
+		cin >> id;
+		scanf(" "); // "removes" the next whitespace from cin
+
+		char name[40];
+		cin.getline(name, sizeof(name));
+		// cout << id << ' ' << name << endl;
+		nameList.push_back(NameValuePair(name, id));
+
+	}
+
+	int numQueries;
+	cin >> numQueries;
+	// cout << "NumQueries: " << numQueries << endl;
+
+	char clearLine[30];
+	cin.getline(clearLine, sizeof(clearLine));
+
+	while (numQueries > 0) {
+		numQueries -= 1;
+		char name[40];
+		cin.getline(name, sizeof(name));
+		// cout << numQueries << ": "<< name << endl;
+
+		auto it = find(nameList.begin(), nameList.end(), name);
+		if (it != nameList.end()) {
+			cout << it->value << endl;
+		}
+	}
+
+	// // Binary Tree Search
+	// map<const char*, int> nameTree;
+	// nameTree[name] = 20;
+	// cout << nameTree[name] << endl;
+	//
+	// // Hash with binary tree search for collision
+	// HashMapTree nameHashMap(10);
+	// nameHashMap.SetValue(name, 20);
+	// cout << nameHashMap.GetValue(name) << endl;
 
 	return 0;
 }
+
+void ListMain() {
+
+}
+
+void TreeMain() {
+
+}
+
+void HashMain() {
+
+}
+
 
 void PrintHashes(string text) {
 	cout << text << endl;
